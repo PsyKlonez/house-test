a906aac89bb23c927839e3fbda68482c10ddf4e5
PsyKlonez <danielyoung261193@gmail.com>
2016-12-11 01:26:40 +0000

    Test Commit.

---
+++ src/housesserver/DatabaseExecutor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package housesserver;
+
+/**
+ *
+ * @author PsY
+ */
+public class DatabaseExecutor {
+    private static String DATABASE_USERNAME = "dba";
+    private static String DATABASE_PASSWORD = "password";
+    private static String DATABASE_ADDRESS = "localhost";
+    private static int DATABASE_PORT = 1337;
+    
+    public DatabaseExecutor() {
+        
+    }
+    
+    private boolean setupConnection() {
+        
+    }
+}
---
+++ src/housesserver/HouseDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package housesserver;
+
+import java.util.List;
+
+/**
+ *
+ * @author PsY
+ */
+public interface HouseDAO {
+    public List<Person> getAllPeople();
+    
+}
